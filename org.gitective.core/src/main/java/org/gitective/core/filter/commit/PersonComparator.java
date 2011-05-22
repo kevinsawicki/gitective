273e0a3c061dff047e0353d453fea90a0e739cd8
Kevin Sawicki <kevinsawicki@gmail.com>
2011-05-22 09:33:42 -0700

    Fix copyright header long lengths.

--- org.gitective.core/src/main/java/org/gitective/core/filter/commit/PersonComparator.java
+++ org.gitective.core/src/main/java/org/gitective/core/filter/commit/PersonComparator.java
@@ -1,10 +1,10 @@
-/*******************************************************************************
+/******************************************************************************
  *  Copyright (c) 2011 Kevin Sawicki <kevinsawicki@gmail.com>
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
  *  http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
+ *****************************************************************************/
 package org.gitective.core.filter.commit;
 
 import java.util.Comparator;
@@ -12,7 +12,7 @@ import java.util.Comparator;
 import org.eclipse.jgit.lib.PersonIdent;
 
 /**
- * 
+ * {@link PersonIdent} comparator that compares the name and email address.
  */
 public class PersonComparator implements Comparator<PersonIdent> {
 
--- org.gitective.core/src/main/java/org/gitective/core/filter/commit/PersonFilter.java
+++ org.gitective.core/src/main/java/org/gitective/core/filter/commit/PersonFilter.java
@@ -1,10 +1,10 @@
-/*******************************************************************************
+/******************************************************************************
  *  Copyright (c) 2011 Kevin Sawicki <kevinsawicki@gmail.com>
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
  *  http://www.eclipse.org/legal/epl-v10.html
- *******************************************************************************/
+ *****************************************************************************/
 package org.gitective.core.filter.commit;
 
 import org.eclipse.jgit.lib.PersonIdent;
