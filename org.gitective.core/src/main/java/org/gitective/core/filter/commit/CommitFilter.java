6ec52e847c7509af895e6406ab566b3a8134ca13
Kevin Sawicki <kevinsawicki@gmail.com>
2011-05-23 21:42:45 -0700

    Correct remaining inconsistent headers.

--- org.gitective.core/src/main/java/org/gitective/core/filter/commit/CommitFilter.java
+++ org.gitective.core/src/main/java/org/gitective/core/filter/commit/CommitFilter.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- *  Copyright (c) 2011 Kevin Sawicki <kevinsawicki@gmail.com>
+ *  Copyright (c) 2011, Kevin Sawicki <kevinsawicki@gmail.com>
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
