762cc014ba35b5d62dfbe687d9bd50ec49f7b2c0
Kevin Sawicki <kevinsawicki@gmail.com>
2011-05-24 18:50:41 -0700

    Add unit test of or commit filter.

---
+++ org.gitective.core/src/test/java/org/gitective/tests/OrTest.java
@@ -0,0 +1,36 @@
+/******************************************************************************
+ *  Copyright (c) 2011, Kevin Sawicki <kevinsawicki@gmail.com>
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *****************************************************************************/
+package org.gitective.tests;
+
+import org.gitective.core.filter.commit.CommitCountFilter;
+import org.gitective.core.filter.commit.CommitLimitFilter;
+import org.gitective.core.filter.commit.OrCommitFilter;
+import org.gitective.core.service.CommitService;
+
+/**
+ * Unit tests of {@link OrCommitFilter}
+ */
+public class OrTest extends GitTestCase {
+
+	/**
+	 * Test second filter in a {@link OrCommitFilter} not being called when the
+	 * first filter matches.
+	 * 
+	 * @throws Exception
+	 */
+	public void testSecondNotCalled() throws Exception {
+		add("file.txt", "test");
+		add("file.txt", "testa");
+		CommitLimitFilter limit = new CommitLimitFilter(1);
+		CommitCountFilter count = new CommitCountFilter();
+		CommitService service = new CommitService(testRepo);
+		service.search(new OrCommitFilter().add(limit).add(count));
+		assertEquals(1, count.getCount());
+	}
+
+}
