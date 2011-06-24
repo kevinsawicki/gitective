7942042db7d998e320b7da67395a81339a5bd88a
Kevin Sawicki <kevinsawicki@gmail.com>
2011-06-24 08:41:19 -0700

    Add more unit tests for or, and, and last commit filters

---
+++ org.gitective.core/src/test/java/org/gitective/tests/AndTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2011 Kevin Sawicki <kevinsawicki@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+package org.gitective.tests;
+
+import org.eclipse.jgit.revwalk.filter.RevFilter;
+import org.gitective.core.filter.commit.AndCommitFilter;
+import org.gitective.core.filter.commit.CommitLimitFilter;
+
+/**
+ * Unit tests of {@link AndCommitFilter}
+ */
+public class AndTest extends GitTestCase {
+
+	/**
+	 * Unit test of {@link AndCommitFilter#clone()}
+	 */
+	public void testClone() {
+		CommitLimitFilter limit = new CommitLimitFilter(1);
+		AndCommitFilter and = new AndCommitFilter(limit);
+		RevFilter clone = and.clone();
+		assertNotNull(clone);
+		assertNotSame(and, clone);
+		assertTrue(clone instanceof AndCommitFilter);
+	}
+
+}
---
+++ org.gitective.core/src/test/java/org/gitective/tests/LastTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2011 Kevin Sawicki <kevinsawicki@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+package org.gitective.tests;
+
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.filter.RevFilter;
+import org.gitective.core.filter.commit.AndCommitFilter;
+import org.gitective.core.filter.commit.CommitLimitFilter;
+import org.gitective.core.filter.commit.LastCommitFilter;
+import org.gitective.core.service.CommitFinder;
+
+/**
+ * Unit tests of {@link LastCommitFilter}
+ */
+public class LastTest extends GitTestCase {
+
+	/**
+	 * Test storing last commit visited
+	 * 
+	 * @throws Exception
+	 */
+	public void testLast() throws Exception {
+		add("a.txt", "a");
+		RevCommit commit2 = add("a.txt", "b");
+		add("a.txt", "c");
+
+		LastCommitFilter last = new LastCommitFilter();
+		assertNull(last.getLast());
+		CommitLimitFilter limit = new CommitLimitFilter(2);
+		AndCommitFilter filter = new AndCommitFilter(limit, last);
+		CommitFinder finder = new CommitFinder(testRepo);
+		finder.setFilter(filter);
+		finder.find();
+		assertEquals(commit2, last.getLast());
+		finder.find();
+		assertEquals(commit2, last.getLast());
+		last.reset();
+		assertNull(last.getLast());
+		limit.reset();
+		finder.find();
+		assertEquals(commit2, last.getLast());
+	}
+
+	/**
+	 * Test of {@link LastCommitFilter#clone()}
+	 */
+	public void testClone() {
+		LastCommitFilter filter = new LastCommitFilter();
+		assertNull(filter.getLast());
+		RevFilter clone = filter.clone();
+		assertNotNull(clone);
+		assertNotSame(filter, clone);
+		assertTrue(clone instanceof LastCommitFilter);
+		assertNull(((LastCommitFilter) clone).getLast());
+	}
+}
