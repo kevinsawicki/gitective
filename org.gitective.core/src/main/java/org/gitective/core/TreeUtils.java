c3e8826da973e7f1b40b5c23839b16c49370c36d
Kevin Sawicki <kevinsawicki@gmail.com>
2011-10-18 15:03:51 -0700

    Add utilities class for interacting with Git trees.
    Helpers were added for creating a tree walk for a commit
    that includes the parent commits and optionally selects
    only differing paths.

---
+++ org.gitective.core/src/main/java/org/gitective/core/TreeUtils.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright (c) 2011 Kevin Sawicki <kevinsawicki@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+package org.gitective.core;
+
+import static org.eclipse.jgit.treewalk.filter.TreeFilter.ANY_DIFF;
+
+import java.io.IOException;
+
+import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.EmptyTreeIterator;
+import org.eclipse.jgit.treewalk.TreeWalk;
+
+/**
+ * Utilities for dealing with trees
+ */
+public abstract class TreeUtils {
+
+	/**
+	 * Get tree associated with commit
+	 *
+	 * @param walk
+	 * @param commit
+	 * @return tree
+	 * @throws IOException
+	 */
+	protected static RevTree getTree(final RevWalk walk, final RevCommit commit)
+			throws IOException {
+		final RevTree tree = commit.getTree();
+		if (tree != null)
+			return tree;
+		walk.parseHeaders(commit);
+		return commit.getTree();
+	}
+
+	/**
+	 * Create tree walk
+	 *
+	 * @param reader
+	 * @param rWalk
+	 * @param commit
+	 * @return tree walk
+	 * @throws IOException
+	 */
+	protected static TreeWalk withParents(final ObjectReader reader,
+			final RevWalk rWalk, final RevCommit commit) throws IOException {
+		final TreeWalk walk = new TreeWalk(reader);
+		final int parentCount = commit.getParentCount();
+		try {
+			switch (parentCount) {
+			case 0:
+				walk.addTree(new EmptyTreeIterator());
+				break;
+			case 1:
+				walk.addTree(getTree(rWalk, commit.getParent(0)));
+				break;
+			default:
+				final RevCommit[] parents = commit.getParents();
+				for (int i = 0; i < parentCount; i++)
+					walk.addTree(getTree(rWalk, parents[i]));
+			}
+			walk.addTree(commit.getTree());
+		} catch (IOException e) {
+			throw new GitException(e, null);
+		}
+		return walk;
+	}
+
+	/**
+	 * Create tree walk with all parent trees
+	 *
+	 * @param repository
+	 * @param commitId
+	 * @return tree walk
+	 */
+	public static TreeWalk withParents(final Repository repository,
+			final AnyObjectId commitId) {
+		if (repository == null)
+			throw new IllegalArgumentException(
+					Assert.formatNotNull("Repository"));
+		if (commitId == null)
+			throw new IllegalArgumentException(
+					Assert.formatNotNull("Commit id"));
+
+		final ObjectReader reader = repository.newObjectReader();
+		final RevWalk walk = new RevWalk(reader);
+		try {
+			return withParents(reader, walk, walk.parseCommit(commitId));
+		} catch (IOException e) {
+			walk.release();
+			throw new GitException(e, null);
+		}
+	}
+
+	/**
+	 * Create tree walk with all parent trees
+	 *
+	 * @param repository
+	 * @param revision
+	 * @return tree walk
+	 */
+	public static TreeWalk withParents(final Repository repository,
+			final String revision) {
+		if (repository == null)
+			throw new IllegalArgumentException(
+					Assert.formatNotNull("Repository"));
+		if (revision == null)
+			throw new IllegalArgumentException(Assert.formatNotNull("Revision"));
+		if (revision.length() == 0)
+			throw new IllegalArgumentException(
+					Assert.formatNotEmpty("Revision"));
+
+		final ObjectReader reader = repository.newObjectReader();
+		final RevWalk walk = new RevWalk(reader);
+		try {
+			return withParents(reader, walk,
+					walk.parseCommit(CommitUtils.resolve(repository, revision)));
+		} catch (IOException e) {
+			walk.release();
+			throw new GitException(e, null);
+		}
+	}
+
+	/**
+	 * Create a tree walk configured to diff the given commit against the parent
+	 * commit(s)
+	 *
+	 * @param repository
+	 * @param commitId
+	 * @return tree walk
+	 */
+	public static TreeWalk diffWithParents(final Repository repository,
+			final AnyObjectId commitId) {
+		final TreeWalk walk = withParents(repository, commitId);
+		walk.setFilter(ANY_DIFF);
+		return walk;
+	}
+
+	/**
+	 * Create a tree walk configured to diff the given commit against the parent
+	 * commit(s)
+	 *
+	 * @param repository
+	 * @param revision
+	 * @return tree walk
+	 */
+	public static TreeWalk diffWithParents(final Repository repository,
+			final String revision) {
+		final TreeWalk walk = withParents(repository, revision);
+		walk.setFilter(ANY_DIFF);
+		return walk;
+	}
+}
---
+++ org.gitective.core/src/test/java/org/gitective/tests/TreeUtilsTest.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2011 Kevin Sawicki <kevinsawicki@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+package org.gitective.tests;
+
+import java.io.IOException;
+
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.storage.file.FileRepository;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.gitective.core.BlobUtils;
+import org.gitective.core.TreeUtils;
+import org.junit.Test;
+
+/**
+ * Unit tests of {@link TreeUtils}
+ */
+public class TreeUtilsTest extends GitTestCase {
+
+	/**
+	 * Parent tree walk with null repository
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void withParentsNullRepository() {
+		TreeUtils.withParents(null, ObjectId.zeroId());
+	}
+
+	/**
+	 * Parent tree walk with null repository
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void withParentsNullRepository2() {
+		TreeUtils.withParents(null, Constants.MASTER);
+	}
+
+	/**
+	 * Parent tree walk with null object id
+	 *
+	 * @throws IOException
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void withParentsNullId() throws IOException {
+		TreeUtils.withParents(new FileRepository(testRepo), (ObjectId) null);
+	}
+
+	/**
+	 * Parent tree walk with null revision
+	 *
+	 * @throws IOException
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void withParentsNullRevision() throws IOException {
+		TreeUtils.withParents(new FileRepository(testRepo), (String) null);
+	}
+
+	/**
+	 * Parent tree walk with empty revision
+	 *
+	 * @throws IOException
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void withParentsEmptyRevision() throws IOException {
+		TreeUtils.withParents(new FileRepository(testRepo), "");
+	}
+
+	/**
+	 * Diff walk for commit with no parents
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void diffWithNoParents() throws Exception {
+		RevCommit commit = add("test.txt", "content");
+		Repository repo = new FileRepository(testRepo);
+		TreeWalk walk = TreeUtils.diffWithParents(repo, Constants.HEAD);
+		assertNotNull(walk);
+		assertEquals(2, walk.getTreeCount());
+		assertTrue(walk.next());
+		assertEquals("test.txt", walk.getPathString());
+		assertEquals(ObjectId.zeroId(), walk.getObjectId(0));
+		assertEquals(BlobUtils.getId(repo, commit, "test.txt"),
+				walk.getObjectId(1));
+		assertFalse(walk.next());
+	}
+
+	/**
+	 * Diff walk for commit with one parent
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void diffWithOneParent() throws Exception {
+		Repository repo = new FileRepository(testRepo);
+		RevCommit commit1 = add("test.txt", "content");
+		RevCommit commit2 = add("test.txt", "content2");
+		TreeWalk walk = TreeUtils.diffWithParents(repo, Constants.HEAD);
+		assertNotNull(walk);
+		assertEquals(2, walk.getTreeCount());
+		assertTrue(walk.next());
+		assertEquals("test.txt", walk.getPathString());
+		assertEquals(BlobUtils.getId(repo, commit1, "test.txt"),
+				walk.getObjectId(0));
+		assertEquals(BlobUtils.getId(repo, commit2, "test.txt"),
+				walk.getObjectId(1));
+		assertFalse(walk.next());
+	}
+}
