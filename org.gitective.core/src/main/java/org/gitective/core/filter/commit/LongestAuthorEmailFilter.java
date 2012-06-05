f022e888177586aeeeff5df4e391191b73d3d794
Kevin Sawicki <kevinsawicki@gmail.com>
2012-06-04 19:08:16 -0700

    Add filter to track commits with the longest author email

---
+++ org.gitective.core/src/main/java/org/gitective/core/filter/commit/LongestAuthorEmailFilter.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2012 Kevin Sawicki <kevinsawicki@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+package org.gitective.core.filter.commit;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+/**
+ * Filter to track the commit(s) with the longest author email address
+ */
+public class LongestAuthorEmailFilter extends CommitFieldLengthFilter {
+
+	@Override
+	public boolean include(final RevWalk walker, final RevCommit commit)
+			throws IOException {
+		final PersonIdent author = commit.getAuthorIdent();
+		if (author == null)
+			return true;
+		final String email = author.getEmailAddress();
+		if (email == null)
+			return true;
+
+		final int emailLength = email.length();
+		if (emailLength >= length)
+			include(emailLength, commit);
+		return true;
+	}
+
+	/**
+	 * Get the commits with the longest author email address length
+	 *
+	 * @return non-null but possibly empty set of commits
+	 */
+	public Set<RevCommit> getCommits() {
+		return commits;
+	}
+
+	/**
+	 * Get the length of the longest commit author email address visited
+	 *
+	 * @return length or -1 if no commits visited
+	 */
+	public int getLength() {
+		return length;
+	}
+
+	@Override
+	public LongestAuthorEmailFilter clone() {
+		return new LongestAuthorEmailFilter();
+	}
+}
---
+++ org.gitective.core/src/test/java/org/gitective/tests/LongestEmailTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2011 Kevin Sawicki <kevinsawicki@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+package org.gitective.tests;
+
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.gitective.core.CommitFinder;
+import org.gitective.core.filter.commit.LongestAuthorEmailFilter;
+import org.junit.Test;
+
+/**
+ * Unit tests of {@link LongestAuthorEmailFilter}
+ */
+public class LongestEmailTest extends GitTestCase {
+
+	/**
+	 * Verify state when no commits are visited
+	 */
+	@Test
+	public void noCommitsIncluded() {
+		LongestAuthorEmailFilter filter = new LongestAuthorEmailFilter();
+		assertEquals(-1, filter.getLength());
+		assertNotNull(filter.getCommits());
+		assertEquals(0, filter.getCommits().size());
+	}
+
+	/**
+	 * Include commits where only one has the longest email
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void singleLongestEmail() throws Exception {
+		LongestAuthorEmailFilter filter = new LongestAuthorEmailFilter();
+		author = new PersonIdent("a", "a@b.c");
+		add("test.txt", "test1");
+		author = new PersonIdent("a", "a@b.cd");
+		add("test.txt", "test2");
+		author = new PersonIdent("a", "a@b.cde");
+		RevCommit longest = add("test.txt", "test3");
+		author = new PersonIdent("a", "a@b.f");
+		add("test.txt", "test4");
+		new CommitFinder(testRepo).setMatcher(filter).find();
+		assertEquals(7, filter.getLength());
+		assertNotNull(filter.getCommits());
+		assertEquals(1, filter.getCommits().size());
+		assertEquals(longest, filter.getCommits().iterator().next());
+	}
+
+	/**
+	 * Include commits where multiples have the longest email
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void multipleLongestEmails() throws Exception {
+		LongestAuthorEmailFilter filter = new LongestAuthorEmailFilter();
+		author = new PersonIdent("a", "a@b.c");
+		add("test.txt", "test1");
+		author = new PersonIdent("a", "a@b.cd");
+		add("test.txt", "test2");
+		author = new PersonIdent("a", "a@b.cde");
+		RevCommit longest1 = add("test.txt", "test3");
+		author = new PersonIdent("a", "a@b.cf");
+		add("test.txt", "test4");
+		author = new PersonIdent("a", "a@b.cdg");
+		RevCommit longest2 = add("test.txt", "test5");
+		new CommitFinder(testRepo).setMatcher(filter).find();
+		assertEquals(7, filter.getLength());
+		assertNotNull(filter.getCommits());
+		assertEquals(2, filter.getCommits().size());
+		assertTrue(filter.getCommits().contains(longest1));
+		assertTrue(filter.getCommits().contains(longest2));
+	}
+}
