f2b823524a97e55ccf2f26d8289381ac25a162cd
Kevin Sawicki <kevinsawicki@gmail.com>
2012-06-04 19:15:14 -0700

    Add filter to track commits with the shortest author email

---
+++ org.gitective.core/src/main/java/org/gitective/core/filter/commit/ShortestAuthorEmailFilter.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2012 Kevin Sawicki <kevinsawicki@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+package org.gitective.core.filter.commit;
+
+import static java.lang.Integer.MAX_VALUE;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+/**
+ * Filter to track the commit(s) with the shortest author email address
+ */
+public class ShortestAuthorEmailFilter extends CommitFieldLengthFilter {
+
+	/**
+	 * Create shortest author email filter
+	 */
+	public ShortestAuthorEmailFilter() {
+		length = MAX_VALUE;
+	}
+
+	@Override
+	public boolean include(final RevWalk walker, final RevCommit commit)
+			throws IOException {
+		final PersonIdent author = commit.getAuthorIdent();
+		if (author == null)
+			return true;
+		final String email = author.getEmailAddress();
+		final int emailLength = email != null ? email.length() : 0;
+		if (emailLength <= length)
+			include(emailLength, commit);
+		return true;
+	}
+
+	/**
+	 * Get the commits with the shortest author email address length
+	 *
+	 * @return non-null but possibly empty set of commits
+	 */
+	public Set<RevCommit> getCommits() {
+		return commits;
+	}
+
+	/**
+	 * Get the length of the shortest commit author email address visited
+	 *
+	 * @return length or -1 if no commits visited
+	 */
+	public int getLength() {
+		return commits.isEmpty() ? -1 : length;
+	}
+
+	@Override
+	public ShortestAuthorEmailFilter clone() {
+		return new ShortestAuthorEmailFilter();
+	}
+}
---
+++ org.gitective.core/src/test/java/org/gitective/tests/ShortestEmailTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2011 Kevin Sawicki <kevinsawicki@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+package org.gitective.tests;
+
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.gitective.core.CommitFinder;
+import org.gitective.core.filter.commit.LongestAuthorEmailFilter;
+import org.gitective.core.filter.commit.ShortestAuthorEmailFilter;
+import org.junit.Test;
+
+/**
+ * Unit tests of {@link ShortestAuthorEmailFilter}
+ */
+public class ShortestEmailTest extends GitTestCase {
+
+	/**
+	 * Verify state when no commits are visited
+	 */
+	@Test
+	public void noCommitsIncluded() {
+		ShortestAuthorEmailFilter filter = new ShortestAuthorEmailFilter();
+		assertEquals(-1, filter.getLength());
+		assertNotNull(filter.getCommits());
+		assertEquals(0, filter.getCommits().size());
+	}
+
+	/**
+	 * Include commits where only one has the shortest email
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void singleLongestEmail() throws Exception {
+		ShortestAuthorEmailFilter filter = new ShortestAuthorEmailFilter();
+		author = new PersonIdent("a", "a@b.com");
+		add("test.txt", "test1");
+		author = new PersonIdent("a", "a@b.net");
+		add("test.txt", "test2");
+		author = new PersonIdent("a", "a@b.ca");
+		RevCommit shortest = add("test.txt", "test3");
+		author = new PersonIdent("a", "a@b.org");
+		add("test.txt", "test4");
+		new CommitFinder(testRepo).setMatcher(filter).find();
+		assertEquals(6, filter.getLength());
+		assertNotNull(filter.getCommits());
+		assertEquals(1, filter.getCommits().size());
+		assertEquals(shortest, filter.getCommits().iterator().next());
+	}
+
+	/**
+	 * Include commits where multiples have the longest email
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void multipleLongestEmails() throws Exception {
+		ShortestAuthorEmailFilter filter = new ShortestAuthorEmailFilter();
+		author = new PersonIdent("a", "a@b.com");
+		add("test.txt", "test1");
+		author = new PersonIdent("a", "a@b.net");
+		add("test.txt", "test2");
+		author = new PersonIdent("a", "a@b.ca");
+		RevCommit shortest1 = add("test.txt", "test3");
+		author = new PersonIdent("a", "a@b.org");
+		add("test.txt", "test4");
+		author = new PersonIdent("a", "a@b.fr");
+		RevCommit shortest2 = add("test.txt", "test5");
+		new CommitFinder(testRepo).setMatcher(filter).find();
+		assertEquals(6, filter.getLength());
+		assertNotNull(filter.getCommits());
+		assertEquals(2, filter.getCommits().size());
+		assertTrue(filter.getCommits().contains(shortest1));
+		assertTrue(filter.getCommits().contains(shortest2));
+	}
+}
