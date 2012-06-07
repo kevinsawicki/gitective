1f4f534313866cc58fb9c17f98c40df389686a75
Kevin Sawicki <kevinsawicki@gmail.com>
2012-06-07 08:55:15 -0700

    Add unit test for resetting a LongestMessageFilter

--- org.gitective.core/src/test/java/org/gitective/tests/LongestMessageTest.java
+++ org.gitective.core/src/test/java/org/gitective/tests/LongestMessageTest.java
@@ -43,6 +43,27 @@ public class LongestMessageTest extends GitTestCase {
 	}
 
 	/**
+	 * Verify {@link LongestMessageFilter#reset()}
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void reset() throws Exception {
+		LongestMessageFilter filter = new LongestMessageFilter();
+		RevCommit longest = add("test.txt", "test", "test");
+		new CommitFinder(testRepo).setMatcher(filter).find();
+		assertEquals(4, filter.getLength());
+		assertNotNull(filter.getCommits());
+		assertEquals(1, filter.getCommits().size());
+		assertEquals(longest, filter.iterator().next());
+		filter.reset();
+		assertEquals(-1, filter.getLength());
+		assertNotNull(filter.getCommits());
+		assertTrue(filter.getCommits().isEmpty());
+		assertFalse(filter.iterator().hasNext());
+	}
+
+	/**
 	 * Include commits where only one has the longest message
 	 *
 	 * @throws Exception
